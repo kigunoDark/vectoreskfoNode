a9430f60196251175326c06b3ea63b2ab0d4dc91
foxBuka <krushenitskii@gmail.com>
2018-07-08 14:59:51 +0300

    the last version

--- public/js/Main.js
+++ public/js/Main.js
@@ -29,14 +29,45 @@ let behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 
 let ui = H.ui.UI.createDefault(map, defaultLayers, "ru-RU");
 
+let circleArr = {}
+circleArr['Зеленый'] = [1 , new H.map.Circle({lat: 45.041263083875585, lng: 41.91064660361326}, 1000, {style: {fillColor: 'rgba(81, 255, 0, 0.7)'}})];
+circleArr['Желтый'] = [3, new H.map.Circle({lat: 45.030830722064714, lng: 41.95030038168943}, 1000, {style: {fillColor: 'rgba(251, 255, 0, 0.7)'}})];
+circleArr['Красный'] = [5, new H.map.Circle({lat: 45.04769138288362, lng: 41.97759454062498}, 1000, {style: {fillColor: 'rgba(250, 0, 0, 0.7)'}})];
+
+for(key in circleArr) {
+    map.addObject(circleArr[key][1]);
+}
+
 eventMarkerGroup = new H.map.Group();
 map.addObject(eventMarkerGroup);
-let eventMarkerDict = {'Хакатон': ['Ну тип хакатон', 
-                                    new H.map.Marker({lat: 45.04882851375588, lng: 41.983690893254135})]
-};
-console.log(eventMarkerDict);
+let eventMarkerDict = {};
 var eventCoords = null;
 var selectedMarker = null;
+
+function latlng2distance(lat1, long1, lat2, long2) {
+//радиус Земли
+var R = 6372795;
+//перевод коордитат в радианы
+lat1 *= Math.PI / 180;
+lat2 *= Math.PI / 180;
+long1 *= Math.PI / 180;
+long2 *= Math.PI / 180;
+//вычисление косинусов и синусов широт и разницы долгот
+var cl1 = Math.cos(lat1);
+var cl2 = Math.cos(lat2);
+var sl1 = Math.sin(lat1);
+var sl2 = Math.sin(lat2);
+var delta = long2 - long1;
+var cdelta = Math.cos(delta);
+var sdelta = Math.sin(delta);
+//вычисления длины большого круга
+var y = Math.sqrt(Math.pow(cl2 * sdelta, 2) + Math.pow(cl1 * sl2 - sl1 * cl2 * cdelta, 2));
+var x = sl1 * sl2 + cl1 * cl2 * cdelta;
+var ad = Math.atan2(y, x);
+var dist = ad * R; //расстояние между двумя координатами в метрах
+return dist
+}
+
 map.addEventListener('tap', function(evt) {
     eventCoords =  map.screenToGeo(evt.currentPointer.viewportX, evt.currentPointer.viewportY);
     var target = evt.target;
@@ -64,22 +95,80 @@ map.addEventListener('tap', function(evt) {
     }
 });
 
+function circleChange(eventCoords, flag) {
+    for(key in circleArr) {
+        if (circleArr[key][1] != null) {
+            var lat = circleArr[key][1].getCenter().lat;
+        var lng = circleArr[key][1].getCenter().lng;
+        var d = latlng2distance(lat, lng, eventCoords.lat, eventCoords.lng);
+        if(d <= circleArr[key][1].getRadius()) {
+            if(!flag) {
+                circleArr[key][0]--;
+            } else {
+                circleArr[key][0]++;
+            }
+        }
+        }
+        if (circleArr['Зеленый'][0] > 0) {
+               if(circleArr['Зеленый'][0] >= circleArr['Желтый'][0]){
+                circleArr['Зеленый'][1].setStyle({
+                    fillColor : 'rgba(251, 255, 0, 0.7)'
+                });
+                circleArr['Желтый'][1].setStyle({
+                    fillColor : 'rgba(81, 255, 0, 0.7)'
+                });
+            } else if(circleArr['Зеленый'][0] < circleArr['Желтый'][0]){
+                circleArr['Зеленый'][1].setStyle({
+                    fillColor : 'rgba(81, 255, 0, 0.7)'
+                });
+                circleArr['Желтый'][1].setStyle({
+                    fillColor : 'rgba(251, 255, 0, 0.7)'
+                });
+            } 
+        }
+
+        if(circleArr['Желтый'][0] > circleArr['Красный'][0]){
+            circleArr['Желтый'][1].setStyle({
+                fillColor : 'rgba(250, 0, 0, 0.7)'
+            });
+            circleArr['Красный'][1].setStyle({
+                fillColor : 'rgba(251, 255, 0, 0.7)'
+            });
+        }  else if (circleArr['Желтый'][0] < circleArr['Красный'][0]){
+            circleArr['Желтый'][1].setStyle({
+                fillColor : 'rgba(251, 255, 0, 0.7)'
+            });
+            circleArr['Красный'][1].setStyle({
+                fillColor : 'rgba(250, 0, 0, 0.7)'
+            });
+        }
+
+        if(circleArr['Зеленый'][0] == 0){
+            map.removeObject(circleArr['Зеленый'][1]);
+            circleArr['Зеленый'][0] = -1;
+            circleArr['Зеленый'][1] = null;
+        }
+        $('#eventMarkerWindow').modal('hide');
+    }
+}
+
 function saveBtn() {
     $('#eventWindow').modal('hide');
+    circleChange(eventCoords, true);
     marker = new H.map.Marker(eventCoords);
     marker.label = document.getElementById("eventName").value;
     eventMarkerGroup.addObject(marker);
     eventMarkerDict[marker.label] = [document.getElementById("eventDesc").value, marker];
     document.getElementById('eventForm').reset();
-    console.log(eventMarkerDict);
+    if(circleArr['Зеленый'][0] == -1){
+       circleArr['Зеленый'][1] = new H.map.Circle(eventCoords, 1000, {style: {fillColor: 'rgba(81, 255, 0, 0.7)'}})
+        map.addObject(circleArr['Зеленый'][1]);
+    }
 }
 
 var searchVal = null;
 function searchInput() {
     searchVal = document.getElementById("searchInput").value;
-    if (document.getElementById("eventSearch").checked) {
-
-    }
     geocode(platform);
 }
 
@@ -125,10 +214,8 @@ function onError(error) {
     alert('Connection error... Try again later');
 }
 
-var searchDict = {};
 function addLocationsToMap(locations){
-    var searchGroup = new H.map.Group(),
-        position,
+    var position,
         i;
 
     if (locations[0].location != undefined) {
@@ -137,10 +224,7 @@ function addLocationsToMap(locations){
                 lat: locations[i].location.displayPosition.latitude,
                 lng: locations[i].location.displayPosition.longitude
             };
-            marker = new H.map.Marker(position);
-            marker.label = locations[i].location.address.label;
-            searchGroup.addObject(marker);
-            searchDict[marker.label] = marker;
+            
         }
     } else {
         for (i = 0;  i < locations.length; i += 1) {
@@ -148,29 +232,53 @@ function addLocationsToMap(locations){
                 lat: locations[i].place.locations[0].displayPosition.latitude,
                 lng: locations[i].place.locations[0].displayPosition.longitude
             };
-            marker = new H.map.Marker(position);
-            marker.label = locations[i].place.locations[0].name;
-            searchGroup.addObject(marker);
-            searchDict[marker.label] = marker;
+            
         }
     }
     
     // Add the locations group to the map
-    map.addObject(searchGroup);
-    map.setCenter(searchGroup.getBounds().getCenter());
+    map.setCenter(position);
+    map.setZoom(16);
 }
 
 function deleteMarkerBtn() {
+    console.log(selectedMarker);
     if (selectedMarker == undefined) {
-
+        eventMarkerGroup.removeObject(eventMarkerDict[document.querySelector('h5#eventMarkerTitle').innerText][1]);
+        circleChange(eventMarkerDict[document.querySelector('h5#eventMarkerTitle').innerText][1].getPosition(), false);
+        delete eventMarkerDict[document.querySelector('h5#eventMarkerTitle').innerText];
+        console.log(eventMarkerDict);
     } else {
         eventMarkerGroup.removeObject(eventMarkerDict[selectedMarker][1]);
+        circleChange(eventMarkerDict[selectedMarker][1].getPosition(), false);
         delete eventMarkerDict[selectedMarker];
         console.log(eventMarkerDict);
-        $('#eventMarkerWindow').modal('hide');
     }
 }
 
-for (var key in eventMarkerDict) {
-    eventMarkerGroup.addObject(eventMarkerDict[key][1]);
+function getData(ready, url) {
+    var request =  new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status != 404){
+            ready(this.responseText);
+        }
+    }
+    request.send();
+};
+
+let url = 'http://localhost:3000/Kek.json'
+getData(function(jsonData) {
+    jsonText = JSON.parse(jsonData, '');
+    console.log(jsonText);
+
+    for(key in jsonText) {
+
+        eventMarkerDict[key] = [jsonText[key][0], new H.map.Marker(jsonText[key][1])];
+        eventMarkerGroup.addObject(eventMarkerDict[key][1]);
+        console.log(eventMarkerDict)
+    }
+}, url);
+
+function turnOn(){
 }
